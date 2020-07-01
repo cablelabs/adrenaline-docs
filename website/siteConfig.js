c464d20b503d5ea10239d29b5286753582e41e6f
Ashwini Kadam <a.kadam@cablelabs.com>
2020-07-01 14:13:28 -0700

    Updated copyright section

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -72,7 +72,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} CableLabs and Altran`,
+  copyright: `Copyright © ${new Date().getFullYear()} CableLabs and Altran by CapGemini `,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
