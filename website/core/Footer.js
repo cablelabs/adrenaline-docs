f5bca5831f99ad3dae31ce9b9d930456204fc851
Ashwini Kadam <a.kadam@cablelabs.com>
2020-07-08 20:51:56 -0700

    Removed banner

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -145,46 +145,46 @@ class Footer extends React.Component {
           />
         </a> */}
         <section className="copyright">{this.props.config.copyright}</section>
-        <div id="mainBanner" ref='mainBanner'>
-          <div id='cookie-banner'  style={{
-            visibility: this.state.showCookie == false ? 'hidden':'visible',
-            position: 'fixed',
-            bottom: '20px',
-            left: '25%',
-            right: '25%',
-            width: '50%',
-            margin:'20px',
-            padding: '15px 15px 15px 15px',
-            display: 'flex',
-            alignItems: 'left',
-            justifyContent: 'space-evenly',
-            backgroundColor: '#070303',
-            color: 'white',
-            borderRadius: '5px',
-            fontSize:'15px',
-            fontFamily: 'soleto,sans-serif',
-            lineHeight: '1.25em',
-            boxShadow: '0 0 2px 1px rgba(0, 0, 0, 0.2)'
-          }}>
-            <p style={{
-              marginTop:'15px'
-            }}>CableLabs.com uses cookies to provide you the best experience. <a style={{color:'grey'}} href="https://www.cablelabs.com/privacy-policy" target="_blank">Learn More</a></p>.
-            <div>
-            <button  onClick={this.cookieConsentHandler()} style={{
-              backgroundColor: 'red',
-              border: 'none',
-              color: 'white',
-              padding: '15px 32px',
-              textAlign: 'center',
-              textDecoration: 'none',
-              display: 'inline-block',
-              fontSize: '15px',
-              margin: '4px 2px',
-              cursor: 'pointer'
-            }}>Got It!</button>
-            </div>
-          </div>
-        </div>
+        {/*<div id="mainBanner" ref='mainBanner'>*/}
+        {/*  <div id='cookie-banner'  style={{*/}
+        {/*    visibility: this.state.showCookie == false ? 'hidden':'visible',*/}
+        {/*    position: 'fixed',*/}
+        {/*    bottom: '20px',*/}
+        {/*    left: '25%',*/}
+        {/*    right: '25%',*/}
+        {/*    width: '50%',*/}
+        {/*    margin:'20px',*/}
+        {/*    padding: '15px 15px 15px 15px',*/}
+        {/*    display: 'flex',*/}
+        {/*    alignItems: 'left',*/}
+        {/*    justifyContent: 'space-evenly',*/}
+        {/*    backgroundColor: '#070303',*/}
+        {/*    color: 'white',*/}
+        {/*    borderRadius: '5px',*/}
+        {/*    fontSize:'15px',*/}
+        {/*    fontFamily: 'soleto,sans-serif',*/}
+        {/*    lineHeight: '1.25em',*/}
+        {/*    boxShadow: '0 0 2px 1px rgba(0, 0, 0, 0.2)'*/}
+        {/*  }}>*/}
+        {/*    <p style={{*/}
+        {/*      marginTop:'15px'*/}
+        {/*    }}>CableLabs.com uses cookies to provide you the best experience. <a style={{color:'grey'}} href="https://www.cablelabs.com/privacy-policy" target="_blank">Learn More</a></p>.*/}
+        {/*    <div>*/}
+        {/*    <button  onClick={this.cookieConsentHandler()} style={{*/}
+        {/*      backgroundColor: 'red',*/}
+        {/*      border: 'none',*/}
+        {/*      color: 'white',*/}
+        {/*      padding: '15px 32px',*/}
+        {/*      textAlign: 'center',*/}
+        {/*      textDecoration: 'none',*/}
+        {/*      display: 'inline-block',*/}
+        {/*      fontSize: '15px',*/}
+        {/*      margin: '4px 2px',*/}
+        {/*      cursor: 'pointer'*/}
+        {/*    }}>Got It!</button>*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
       </footer>
     );
   }
